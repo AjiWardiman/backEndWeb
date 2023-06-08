51e4c54a1e3e8dd25f9b89f95fc9d0f82da9e307
AjiWardiman <ajiwardiman@gmail.com>
2023-06-08 14:27:38 +0700

    first commit

---
+++ routes/productRoute.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+  addToWishlist,
+  rating,
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const { uploadImages } = require("../controller/uploadCtrl");
+const { blogImgResize, uploadPhoto } = require("../middlewares/uploadImage");
+const router = express.Router();
+
+router.put(
+  "/upload/:id",
+  authMiddleware,
+  isAdmin,
+  uploadPhoto.array("images", 2),
+  blogImgResize,
+  uploadImages
+);
+router.post("/", authMiddleware, isAdmin, createProduct);
+
+router.get("/:id", getaProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
+
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+
+router.get("/", getAllProduct);
+
+module.exports = router;
